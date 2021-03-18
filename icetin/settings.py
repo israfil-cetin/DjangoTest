f4a9470e365b431020693f12bda38d146f80e2fb
israfil çetin <icetin93@gmail.com>
2021-03-18 20:20:25 +0000

    some settings for site

--- icetin/settings.py
+++ icetin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o851tnel@&#_k*tm54-gl9j&+=d!t$bb68uasawg7voa40s2+2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["yemek.pythonanywhere.com","yemek.pythonanywhere.com/admin",]
 
 
 # Application definition
@@ -124,11 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 
